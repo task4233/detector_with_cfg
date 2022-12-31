80ca353e364756f8b60de2d6aad5170c2b9fd7d5
task4233 <29667656+task4233@users.noreply.github.com>
2022-12-31 09:06:29 +0900

    feat: add plan to implement

--- detector_with_cfg/converter.py
+++ detector_with_cfg/converter.py
@@ -23,36 +23,29 @@ class Covnerter():
         try:
             self.__validate_paths()
             self.__load_jsons()
-
-            # やりたいことは、all_apiの[0,1,0,0,1]みたいなのを作ること
-            api_usages = []
-            api_frequencies = []
-            for api_freq in self.api_freqs:
-                # 各APKのAPI_USAGE listを作る
-                api_usage = [0] * len(self.all_apis)
-                api_frequency = [0] * len(self.all_apis)
-                for api_name, occurrences in api_freq.items():
-                    # api_usage_idx
-                    api_usage_idx = self.all_apis[api_name]
-                    api_usage[api_usage_idx] = 1
-                    api_frequency[api_usage_idx] = occurrences
-                api_usages.append(api_usage)
-                api_frequencies.append(api_frequency)
-
-            with open(self.api_usages_path, 'w') as f:
-                json.dump(api_usages, f)
-            with open(self.api_frequencies_path, 'w') as f:
-                json.dump(api_frequencies, f)
+            self.__convert()
+            self.__save_jsons()
+            return self.api_usages, self.api_frequencies
         except Exception:
             traceback.print_exc()
 
-    def __validate_paths(self) -> None:
-        if not os.path.exists(self.all_apis_path):
-            mes = f"{self.all_apis_path} does not exist"
-            raise FileNotFoundError(mes)
-        if not os.path.exists(self.api_freqs_path):
-            mes = f"{self.api_freqs_path} does not exist"
-            raise FileNotFoundError(mes)
+    def __convert(self) -> None:
+        """
+        make one-hot vector from universal set of all apis in given APKs.
+        """
+        self.api_usages = []
+        self.api_frequencies = []
+        for api_freq in self.api_freqs:
+            # 各APKのAPI_USAGE listを作る
+            api_usage = [0] * len(self.all_apis)
+            api_frequency = [0] * len(self.all_apis)
+            for api_name, occurrences in api_freq.items():
+                # api_usage_idx
+                api_usage_idx = self.all_apis[api_name]
+                api_usage[api_usage_idx] = 1
+                api_frequency[api_usage_idx] = occurrences
+            self.api_usages.append(api_usage)
+            self.api_frequencies.append(api_frequency)
 
     def __load_jsons(self) -> None:
         # load jsons
@@ -63,3 +56,18 @@ class Covnerter():
         with open(self.api_freqs_path, 'r') as f:
             # api_freqs: List<Map<String, Integer>> <=> List<Map<api_name, occurrences>>
             self.api_freqs = json.load(f)
+
+    def __save_jsons(self) -> None:
+        with open(self.api_usages_path, 'w') as f:
+            json.dump(self.api_usages, f)
+        with open(self.api_frequencies_path, 'w') as f:
+            json.dump(self.api_frequencies, f)
+
+
+    def __validate_paths(self) -> None:
+        if not os.path.exists(self.all_apis_path):
+            mes = f"{self.all_apis_path} does not exist"
+            raise FileNotFoundError(mes)
+        if not os.path.exists(self.api_freqs_path):
+            mes = f"{self.api_freqs_path} does not exist"
+            raise FileNotFoundError(mes)
--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     api_usages_path = 'output/api_usages.json'
     api_frequencies_path = 'output/api_frequencies.json'
 
-    converter.Covnerter(
+    api_usages, api_frequences = converter.Covnerter(
         all_apis_path,
         api_freqs_path,
         api_usages_path,
         api_frequencies_path,
     ).convert()
+
+    # 説明変数と目的変数に分ける
+    # 説明変数: (API Usage, API Frequency, API Sequence)
+    # 目的変数: (benign/malicious)
+    # 学習とテスト用データセットに分割する
+    # 決定木モデルの作成と予測
 
 if __name__ == '__main__':
     main()
