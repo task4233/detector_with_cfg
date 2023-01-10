326ad0812f1ee72fec92a59dc7040b54e4266a9d
task4233 <29667656+task4233@users.noreply.github.com>
2023-01-10 10:38:12 +0900

    feat: add results for 2, 3, 4

--- detector_with_cfg/classifier.py
+++ detector_with_cfg/classifier.py
@@ -8,6 +8,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
 
 import json
 matplotlib.use('TkAgg')
@@ -20,6 +24,8 @@ class Classifier:
         self.api_usages_path = 'output/api_usages.json'
         self.api_frequencies_path = 'output/api_frequencies.json'
         self.api_sequences_path = 'output/api_sequences.json'
+
+        self.depth = 4
 
         # self.api_usages, self.api_freqs = converter.Covnerter(
         #     self.all_apis_path,
@@ -62,15 +68,37 @@ class Classifier:
             df_x, df_y, random_state=1)
 
         # 決定木モデルの作成
-        model = tree.DecisionTreeClassifier(max_depth=3, random_state=1)
+        model = tree.DecisionTreeClassifier(max_depth=self.depth, random_state=1)
         # model = lightgbm.LGBMRegressor()
         model.fit(train_x, train_y)
 
         # 予測
-        model.predict(test_x)
+        pred_y = model.predict(test_x)
         score = model.score(test_x, test_y)
         print(f"score({name}): {score}")
 
         plt.figure(figsize=(15, 10))
         plot_tree(model, feature_names=train_x.columns, class_names=True, filled=True)
-        plt.savefig(f"{name}.pdf")
+        plt.savefig(f"{name}_{self.depth}.pdf")
+
+        self.drawing_confusion_matrix(test_y, pred_y, name)
+        self.calculation_evaluations(test_y, pred_y)
+
+
+    def drawing_confusion_matrix(self, y: pd.Series, pre: np.ndarray, name: str) -> None: 
+        confmat = confusion_matrix(y, pre)
+        fig, ax = plt.subplots(figsize=(5, 5))
+        ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+        for i in range(confmat.shape[0]):
+            for j in range(confmat.shape[1]):
+                ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+        plt.title('Predicted value')
+        plt.ylabel('Measured value')
+        plt.rcParams["font.size"] = 15
+        plt.tight_layout() 
+        plt.savefig(f"{name}_confusion_matrix_{self.depth}.pdf")
+
+    def calculation_evaluations(self, y: pd.Series, pre: np.ndarray) -> None:
+        print('Acc: {:.3f}'.format(metrics.accuracy_score(y, pre)))
+        print('Pre: {:.3f}'.format(metrics.precision_score(y, pre)))
+        print('Rec: {:.3f}'.format(metrics.recall_score(y, pre)))
